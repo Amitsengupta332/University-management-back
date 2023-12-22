da2c0ae44f03c39dc89d8c7ab021e70c2bda4435
Amitsen11 <89931804+Amitsen11@users.noreply.github.com>
2023-12-22 23:41:55 +0600

    fix spelling mistake

--- src/app/modules/semesterRegistration/semesterRegistration.service.ts
+++ src/app/modules/semesterRegistration/semesterRegistration.service.ts
@@ -1,6 +1,40 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
+import httpStatus from 'http-status';
+import AppError from '../../errors/AppError';
+import { AcademicSemester } from '../academicSemester/academicSemester.model';
+import { TSemesterRegistration } from './semesterRegistration.interface';
+import { SemesterRegistration } from './semesterRegistration.model';
 
-const createSemesterRegistrationIntoDB = async () => {};
+const createSemesterRegistrationIntoDB = async (
+  payload: TSemesterRegistration,
+) => {
+  const academicSemester = payload?.academicSemester;
+
+  // check if the semester is exists
+  const isAcademicSemesterExists =
+    await AcademicSemester.findById(academicSemester);
+
+  if (!isAcademicSemesterExists) {
+    throw new AppError(
+      httpStatus.NOT_FOUND,
+      'This academic semester not found !',
+    );
+  }
+
+  // check if the semester is Already Registered?
+  const isSemesterRegistrationExists = await SemesterRegistration.findOne({
+    academicSemester,
+  });
+
+  if (isSemesterRegistrationExists) {
+    throw new AppError(
+      httpStatus.CONFLICT,
+      'This semester is already registered!',
+    );
+  }
+  const result = await SemesterRegistration.create(payload);
+  return result;
+};
 
 const getAllSemesterRegistrationsFromDB = async () => {};
 
